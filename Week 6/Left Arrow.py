73a6ef7541278d1579bba64bdecb1a1bd6228baf
Sirachet Chotthakunanan <140479365+psrct@users.noreply.github.com>
2023-08-12 18:59:43 +0700

    Delete Week 6 directory

--- Week 6/Left Arrow.py
+++
@@ -1,8 +0,0 @@
-'''Left Arrow'''
-def arrow(width, high):
-    '''Left Arrow'''
-    middle = high // 2
-    for i in range(high):
-        print(" " * abs(middle - i) + "*" * width)
-
-arrow(int(input()), int(input()))
