8e698cd71fa678f7999cf5ac43a3c7c5166a122d
Sirachet Chotthakunanan <sirachet.c@gmail.com>
2023-08-23 23:20:17 +0700

    Brick

--- Week3 If else/Brick Bridge.py
+++ Week3 If else/Brick Bridge.py
@@ -1,5 +1,14 @@
 '''Brick Bridge'''
-def main(numa, numb, goal):
+def main(small, big, goal):
     '''Brick Bridge'''
-    
+    if goal // 5 >= big:
+        goal = goal - (big * 5)
+    else:
+        goal = goal - ((goal // 5)*5)
+
+    if goal <= small:
+        print(goal)
+    else:
+        print("-1")
+
 main(int(input()), int(input()), int(input()))
